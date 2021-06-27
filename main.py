4a404223c4b2c210e7e5c6fd8eb31ab8f0294f13
Asaf <asaf.tavor@gmail.com>
2021-06-27 21:58:26 +0300

    refactors

--- main.py
+++ main.py
@@ -1,33 +1,35 @@
 import os
 import json
+from os.path import join, isdir
+
 from constants import INTERVAL_OPTS
-from data_fetchers.coin_data_fetcher import COINS_DATA_PATH
-from strategies.examples import ThreeEmaStochRsi
+from data_fetchers.coin_data_fetcher import COINS_DATA_PATH, STOCKS_DATA_PATH
+from strategies.examples import ThreeEmaStochRsi, ParabolicSAR_SMA200, IchiStcCmf, SuperTrendStochEma200, \
+    StochRsiMACD, ParabolicSAR_EMA200_MACD, DecisionTreeAI
 from use_local_data import use_local_data
 from strategy_tester import StrategyTester
 
+RESULTS_FOLDER = join(".", "test_results")
 
-RESULTS_FOLDER = ".\\test_results"
 
 def main():
     strategy = ThreeEmaStochRsi
-    new_dir_path = RESULTS_FOLDER + "\\" + strategy.__name__
+    new_dir_path = join(RESULTS_FOLDER, strategy.__name__)
 
-    if not os.path.isdir(new_dir_path):
-        os.mkdir(new_dir_path)
+    if not isdir(new_dir_path):
+        os.makedirs(new_dir_path)
 
     for interval in INTERVAL_OPTS:
-        coins_data = use_local_data(interval, COINS_DATA_PATH)
-        strat_tester = StrategyTester(strategy, coins_data, interval=interval)
+        data = use_local_data(interval, COINS_DATA_PATH)
+        strat_tester = StrategyTester(strategy, data, interval=interval)
         analysis = strat_tester.get_analysis()
         print(interval, analysis["daily_increase_percentage"], analysis["total_amount"], analysis["starting_amount"])
 
-        with open(new_dir_path + f"\\{interval}.json", "w") as f:
-            json.dump(analysis, f, indent=4)
+    with open(join(new_dir_path, f"{interval}.json"), "w") as f:
+        json.dump(analysis, f, indent=4)
 
-        strat_tester.save_plot(new_dir_path + f"\\{interval}.png")
+    strat_tester.save_plot(join(new_dir_path, f"{interval}.png"))
 
 
 if __name__ == '__main__':
     main()
-
